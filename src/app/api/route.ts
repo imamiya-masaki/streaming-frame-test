40c5b0721ea76020dac8e75b232d66de81d57b9c
imamiya-masaki <imamiyagit@gmail.com>
2023-11-19 06:54:32 +0900

    init

---
+++ src/app/api/route.ts
@@ -0,0 +1,16 @@
+
+async function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const ms = Number(searchParams.get('ms'))
+    const isError = searchParams.get('is_error') ?? false
+    await sleep(ms)
+    if (isError) {
+      return Response.error()
+    } else {
+      return Response.json({"response": "ok"})
+    }
+  }
